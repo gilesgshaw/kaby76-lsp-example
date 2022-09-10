e2e94a6fc5397c217a3cf8434a587a507012f24d
Giles Shaw <gilesgshaw@gmail.com>
2022-09-10 22:33:56 +0100

    remove something dodgy

--- Sample/VsCode/clean.sh
+++ Sample/VsCode/clean.sh
@@ -3,6 +3,6 @@ rm -rf node_modules
 rm -rf node_modules
 rm -f package-lock.json
 rm -rf out
-rm -rf 'c:/Users/kenne/AppData/Roaming/Code'
+#rm -rf 'c:/Users/kenne/AppData/Roaming/Code'
 rm -rf server/net472
 rm -rf *.vsix
